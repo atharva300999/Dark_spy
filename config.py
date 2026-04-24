09b5c9c1d5a7685cdd6c5c309ce67c66cf0265f5
atharva300999 <atharb922@gmail.com>
2026-04-24 15:23:21 +0530

    Add configuration settings for bot and APIs

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "8743017124:AAFty35V7dJCOEQhTYRfaqB7YjOtqRfeXlI"
+ADMIN_USER_ID = 6798566345
+
+# Private Channel Invite Link (user join korbe)
+PRIVATE_CHANNEL_LINK = "https://t.me/+kgNayF9DcgAzYjk1"
+
+API_TG_TO_NUMBER = "https://tgchatid.vercel.app/api/lookup?number={query}"
+API_GST = "https://toxic-eight.vercel.app/gst?number={query}"
+API_VEHICLE = "https://toxic-vehical.vercel.app/api/vehicle-info?rc={query}"
+API_IFSC = "https://ab-ifscinfoapi.vercel.app/info?ifsc={query}"
